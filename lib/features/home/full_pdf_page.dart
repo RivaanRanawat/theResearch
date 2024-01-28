88e531f1fb60635fc2cf6388d3ddcaeac9099cd3
Rivaan Ranawat <namanrivaan@gmail.com>
2024-01-27 20:37:47 -0500

    full page pdf

---
+++ lib/features/home/full_pdf_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:research/models/research_model.dart';
+import 'package:research/theme/pallete.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class FullPDFPage extends StatelessWidget {
+  final ResearchModel researchModel;
+  const FullPDFPage({
+    super.key,
+    required this.researchModel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            SfPdfViewer.network(
+              researchModel.pdfUrl,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.4),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      child: MaterialButton(
+                        elevation: 0,
+                        height: 50,
+                        onPressed: () {
+                          // loginUser(type: LoginType.google, context: context);
+                        },
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(FontAwesomeIcons.dollarSign),
+                            SizedBox(width: 10),
+                            Text(
+                              'Fund Research',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      child: MaterialButton(
+                        elevation: 0,
+                        height: 50,
+                        onPressed: () {
+                          // loginUser(type: LoginType.google, context: context);
+                        },
+                        color: Pallete.logoGreen,
+                        textColor: Colors.white,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(FontAwesomeIcons.comment),
+                            SizedBox(width: 10),
+                            Text(
+                              'Discuss',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/post_card.dart
+++ lib/features/home/post_card.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:font_awesome_flutter/font_awesome_flutter.dart";
+import "package:research/features/home/full_pdf_page.dart";
 import "package:research/models/research_model.dart";
 import "package:syncfusion_flutter_pdfviewer/pdfviewer.dart";
 
@@ -26,88 +28,125 @@ class _PostCardState extends State<PostCard>
         borderRadius: const BorderRadius.all(
           Radius.circular(10),
         ),
-        child: Stack(
-          children: [
-            SfPdfViewer.network(
-              widget.researchModel.pdfUrl,
-            ),
-            Positioned(
-              left: 5,
-              top: 5,
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.4),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: Text(
-                  widget.researchModel.title,
-                  style: const TextStyle(
-                    fontSize: 22,
-                    color: Colors.white,
-                  ),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => FullPDFPage(
+                  researchModel: widget.researchModel,
                 ),
               ),
-            ),
-            if (widget.researchModel.summary.isNotEmpty)
-              Positioned(
-                right: 5,
-                top: 5,
+            );
+          },
+          child: Stack(
+            children: [
+              SfPdfViewer.network(
+                widget.researchModel.pdfUrl,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
                 child: GestureDetector(
                   onTap: () {
-                    showModalBottomSheet(
-                      useRootNavigator: true,
-                      context: context,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.vertical(
-                          top: Radius.circular(
-                            30.0,
-                          ),
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => FullPDFPage(
+                          researchModel: widget.researchModel,
                         ),
                       ),
-                      builder: (newContext) {
-                        return SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.6,
-                          width: MediaQuery.of(context).size.width,
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                              left: 20.0,
-                              right: 20.0,
-                              top: 10,
-                              bottom: 10,
-                            ),
-                            child: SingleChildScrollView(
-                              child: Column(
-                                children: [
-                                  const Text(
-                                    'Summary',
-                                    style: TextStyle(
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    widget.researchModel.summary.trim(),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        );
-                      },
                     );
                   },
                   child: Container(
+                    margin: const EdgeInsets.only(right: 5),
                     padding: const EdgeInsets.all(8),
                     decoration: BoxDecoration(
                       color: Colors.black.withOpacity(0.4),
                       borderRadius: BorderRadius.circular(5),
                     ),
-                    child: const Icon(Icons.info_rounded),
+                    child: const Icon(
+                      FontAwesomeIcons.arrowRight,
+                      size: 20,
+                    ),
                   ),
                 ),
-              )
-          ],
+              ),
+              Positioned(
+                left: 5,
+                top: 5,
+                child: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.4),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(
+                    widget.researchModel.title,
+                    style: const TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              if (widget.researchModel.summary.isNotEmpty)
+                Positioned(
+                  right: 5,
+                  top: 5,
+                  child: GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet(
+                        useRootNavigator: true,
+                        context: context,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(
+                              30.0,
+                            ),
+                          ),
+                        ),
+                        builder: (newContext) {
+                          return SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.6,
+                            width: MediaQuery.of(context).size.width,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                left: 20.0,
+                                right: 20.0,
+                                top: 10,
+                                bottom: 10,
+                              ),
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  children: [
+                                    const Text(
+                                      'Summary',
+                                      style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      widget.researchModel.summary.trim(),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.4),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: const Icon(Icons.info_rounded),
+                    ),
+                  ),
+                )
+            ],
+          ),
         ),
       ),
     );
