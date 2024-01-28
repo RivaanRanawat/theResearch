094e93b0b5ec22a42b2b07d462c8f36f2a2daaba
Rivaan Ranawat <namanrivaan@gmail.com>
2024-01-28 02:48:02 -0500

    displaying research fund button only

--- lib/features/home/full_pdf_page.dart
+++ lib/features/home/full_pdf_page.dart
@@ -34,106 +34,108 @@ class FullPDFPage extends ConsumerWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Container(
-                      padding: const EdgeInsets.all(8),
-                      child: MaterialButton(
-                        elevation: 0,
-                        height: 50,
-                        onPressed: () {
-                          final user = ref.read(currentUserModelProvider)!;
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              var fundingValue = 0.0;
-                              return StatefulBuilder(
-                                builder: (context, setS) => Dialog(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(25.0),
-                                    child: Column(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        const Text(
-                                          "How much would you like to fund this research?",
-                                          style: TextStyle(
-                                            fontSize: 18,
-                                          ),
-                                        ),
-                                        Slider(
-                                          value: fundingValue,
-                                          min: 0,
-                                          max: user.funding.toDouble(),
-                                          thumbColor: Colors.blue,
-                                          activeColor:
-                                              Colors.blue.withOpacity(0.7),
-                                          divisions: user.funding,
-                                          label: '\$$fundingValue',
-                                          onChanged: (val) {
-                                            setS(() {
-                                              fundingValue = val;
-                                            });
-                                          },
-                                        ),
-                                        MaterialButton(
-                                          elevation: 0,
-                                          minWidth: double.maxFinite,
-                                          height: 50,
-                                          onPressed: () {
-                                            ref
-                                                .read(authControllerProvider
-                                                    .notifier)
-                                                .updateFundingUsers(
-                                                  researchModel.uid,
-                                                  fundingValue.toInt(),
-                                                  researchModel.id,
-                                                  context,
-                                                );
-                                          },
-                                          color: Colors.blue,
-                                          textColor: Colors.white,
-                                          shape: const RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.all(
-                                              Radius.circular(5),
+                    if (researchModel.uid !=
+                        ref.watch(currentUserModelProvider)!.uid)
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        child: MaterialButton(
+                          elevation: 0,
+                          height: 50,
+                          onPressed: () {
+                            final user = ref.read(currentUserModelProvider)!;
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                var fundingValue = 0.0;
+                                return StatefulBuilder(
+                                  builder: (context, setS) => Dialog(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(25.0),
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          const Text(
+                                            "How much would you like to fund this research?",
+                                            style: TextStyle(
+                                              fontSize: 18,
                                             ),
                                           ),
-                                          child: Text(
-                                            'Fund \$$fundingValue',
-                                            style: const TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 16,
+                                          Slider(
+                                            value: fundingValue,
+                                            min: 0,
+                                            max: user.funding.toDouble(),
+                                            thumbColor: Colors.blue,
+                                            activeColor:
+                                                Colors.blue.withOpacity(0.7),
+                                            divisions: user.funding,
+                                            label: '\$$fundingValue',
+                                            onChanged: (val) {
+                                              setS(() {
+                                                fundingValue = val;
+                                              });
+                                            },
+                                          ),
+                                          MaterialButton(
+                                            elevation: 0,
+                                            minWidth: double.maxFinite,
+                                            height: 50,
+                                            onPressed: () {
+                                              ref
+                                                  .read(authControllerProvider
+                                                      .notifier)
+                                                  .updateFundingUsers(
+                                                    researchModel.uid,
+                                                    fundingValue.toInt(),
+                                                    researchModel.id,
+                                                    context,
+                                                  );
+                                            },
+                                            color: Colors.blue,
+                                            textColor: Colors.white,
+                                            shape: const RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.all(
+                                                Radius.circular(5),
+                                              ),
+                                            ),
+                                            child: Text(
+                                              'Fund \$$fundingValue',
+                                              style: const TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 16,
+                                              ),
                                             ),
                                           ),
-                                        ),
-                                      ],
+                                        ],
+                                      ),
                                     ),
                                   ),
+                                );
+                              },
+                            );
+                          },
+                          color: Colors.blue,
+                          textColor: Colors.white,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5),
+                            ),
+                          ),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(FontAwesomeIcons.dollarSign),
+                              SizedBox(width: 10),
+                              Text(
+                                'Fund Research',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
                                 ),
-                              );
-                            },
-                          );
-                        },
-                        color: Colors.blue,
-                        textColor: Colors.white,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(5),
+                              ),
+                            ],
                           ),
                         ),
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Icon(FontAwesomeIcons.dollarSign),
-                            SizedBox(width: 10),
-                            Text(
-                              'Fund Research',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                              ),
-                            ),
-                          ],
-                        ),
                       ),
-                    ),
                     // Stack(
                     //   children: [
                     Container(
