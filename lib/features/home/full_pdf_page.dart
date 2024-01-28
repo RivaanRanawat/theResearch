ddeee70384cdbd4785205487e6284ce02458990e
Rivaan Ranawat <namanrivaan@gmail.com>
2024-01-28 10:40:35 -0500

    bug fixed 2

--- lib/features/home/full_pdf_page.dart
+++ lib/features/home/full_pdf_page.dart
@@ -45,77 +45,82 @@ class FullPDFPage extends ConsumerWidget {
                           elevation: 0,
                           height: 50,
                           onPressed: () {
-                            final user = ref.read(currentUserModelProvider)!;
                             showDialog(
                               context: context,
                               builder: (context) {
                                 var fundingValue = 0.0;
                                 return StatefulBuilder(
-                                  builder: (context, setS) => Dialog(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(25.0),
-                                      child: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          const Text(
-                                            "How much would you like to fund this research?",
-                                            style: TextStyle(
-                                              fontSize: 18,
-                                            ),
-                                          ),
-                                          Slider(
-                                            value: fundingValue,
-                                            min: 0,
-                                            max: user.funding.toDouble() > 0
-                                                ? user.funding.toDouble()
-                                                : 0.1,
-                                            thumbColor: Colors.blue,
-                                            activeColor:
-                                                Colors.blue.withOpacity(0.7),
-                                            divisions: user.funding > 0
-                                                ? user.funding
-                                                : 1,
-                                            label: '\$$fundingValue',
-                                            onChanged: (val) {
-                                              setS(() {
-                                                fundingValue = val;
-                                              });
-                                            },
-                                          ),
-                                          MaterialButton(
-                                            elevation: 0,
-                                            minWidth: double.maxFinite,
-                                            height: 50,
-                                            onPressed: () {
-                                              ref
-                                                  .read(authControllerProvider
-                                                      .notifier)
-                                                  .updateFundingUsers(
-                                                    researchModel.uid,
-                                                    fundingValue.toInt(),
-                                                    researchModel.id,
-                                                    context,
-                                                  );
-                                            },
-                                            color: Colors.blue,
-                                            textColor: Colors.white,
-                                            shape: const RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.all(
-                                                Radius.circular(5),
+                                  builder: (context, setS) {
+                                    final user =
+                                        ref.read(currentUserModelProvider)!;
+                                    return Dialog(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(25.0),
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            const Text(
+                                              "How much would you like to fund this research?",
+                                              style: TextStyle(
+                                                fontSize: 18,
                                               ),
                                             ),
-                                            child: Text(
-                                              'Fund \$$fundingValue',
-                                              style: const TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 16,
+                                            Slider(
+                                              value: fundingValue,
+                                              min: 0,
+                                              max: user.funding.toDouble() > 0
+                                                  ? user.funding.toDouble()
+                                                  : 0.1,
+                                              thumbColor: Colors.blue,
+                                              activeColor:
+                                                  Colors.blue.withOpacity(0.7),
+                                              divisions: user.funding > 0
+                                                  ? user.funding
+                                                  : 1,
+                                              label: '\$$fundingValue',
+                                              onChanged: (val) {
+                                                setS(() {
+                                                  fundingValue = val;
+                                                });
+                                              },
+                                            ),
+                                            MaterialButton(
+                                              elevation: 0,
+                                              minWidth: double.maxFinite,
+                                              height: 50,
+                                              onPressed: () {
+                                                ref
+                                                    .read(authControllerProvider
+                                                        .notifier)
+                                                    .updateFundingUsers(
+                                                      researchModel.uid,
+                                                      fundingValue.toInt(),
+                                                      researchModel.id,
+                                                      context,
+                                                    );
+                                                Navigator.pop(context);
+                                              },
+                                              color: Colors.blue,
+                                              textColor: Colors.white,
+                                              shape:
+                                                  const RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.all(
+                                                  Radius.circular(5),
+                                                ),
+                                              ),
+                                              child: Text(
+                                                'Fund \$$fundingValue',
+                                                style: const TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 16,
+                                                ),
                                               ),
                                             ),
-                                          ),
-                                        ],
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                  ),
+                                    );
+                                  },
                                 );
                               },
                             );
